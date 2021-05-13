f97e64dabcdd75593a3cae5442b590927e6bacd2
Jernej Strazisar <strazisar.jernej@gmail.com>
2021-05-13 12:11:46 +0200

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-recommended",
+        "prettier"
+    ],
+    plugins: [
+        "@typescript-eslint"
+    ],
+    rules: {
+        quotes: ["error", "double"]
+    },
+    ignorePatterns: [
+        ".eslintrc.js",
+        "src/shims-vue.d.ts",
+        "node_modules/"
+    ]
+}
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ The main purpose of this project is to reduce the time necessary to scaffold a n
 * Typescript support
 * [Vite](https://vitejs.dev/)
 * CSS modules and SCSS support
+* Error checking with ESLint and code formatting with Prettier
 
 ## Installation Usage
 
@@ -27,4 +28,14 @@ npm run dev
 To build the project for production you can use:
 ```
 npm run build
+```
+
+To check for errors in your Vue and Typescript files you can use:
+```
+npm run lint:es
+```
+
+To automatically format the code with Prettier you can use:
+```
+npm run format
 ```
